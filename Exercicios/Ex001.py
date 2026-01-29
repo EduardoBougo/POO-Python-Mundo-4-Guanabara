def8901f46225a2658017fd3c0294d382f8f49d7
EduardoBougo <eduardobougo@gmail.com>
2026-01-29 14:25:10 -0300

    Create Ex001.py

---
+++ Exercicios/Ex001.py
@@ -0,0 +1,38 @@
+#Declaração de classes
+class Estudante():
+    def __init__(self): #Método Construtor
+        #Atributos de instância
+        self.nome = ""
+        self.idade = 0
+        self.matriculado = False
+        self.curso = ""
+        self.periodo = 1
+
+    #Métodos de instância
+    
+    def matricular(self):
+        if self.matriculado is False:
+            self.matriculado = True
+            
+    def aprovado(self):
+        self.periodo += 1
+        
+    def mensagem(self):
+        return (f"{self.nome} é um(a) Estudante de Graduação, tem {self.idade} anos, está no {self.periodo} periodo da graduação")
+    
+
+
+
+#Declaração de Objetos
+
+aluno1 = Estudante()
+aluno1.nome = "Eduardo"
+aluno1.idade = 18
+aluno1.curso = "Engenharia da Computação"
+
+print(aluno1.mensagem())
+
+aluno1.aprovado()
+aluno1.aprovado()
+
+print(aluno1.mensagem())

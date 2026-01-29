6d5b5da7b2fa173a1df73b63d9d9f74cddfa92b4
EduardoBougo <eduardobougo@gmail.com>
2026-01-29 14:24:58 -0300

    Create Testes.py

---
+++ Testes.py
@@ -0,0 +1,23 @@
+print("hello world")
+print("Notificação - Instanciar - Métodos - Estado - ")
+print(" Classe: celular")
+print("""\
+Atributos: 
+        Cor
+        Modelo
+        Tamanho
+""")
+print("""\
+Metodos:
+        Ligar()
+        Desligar()
+        Reiniciar()
+        Vibrar()
+""")
+
+
+
+
+
+
+

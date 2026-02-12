b61781ea18eccd1ecce4d59f63e9c63585e8339e
EduardoBougo <eduardobougo@gmail.com>
2026-02-12 16:37:06 -0300

    a

--- Exercicios/Ex002/Ex002.py
+++ Exercicios/Ex002/Ex002.py
@@ -1,6 +1,35 @@
 #Declaração de classes
 class Estudante():
+    """
+    Docstring para Estudante
+    Esta classe tem como objetivo criar um modelo de estudante, com atributos e métodos relacionados a um estudante de graduação.
+    
+    Atributos:  
+    - nome (str): Nome do Estudante
+    - idade (int): Idade do estudante
+    - matriculado (bool): Atributo para saber se o estudante esta ou não matriculado
+    - curso (str): Nome do curso do estudante
+    - periodo (int): Periodo em que o estudante está cursando
+    
+    Métodos:
+    - matricular(): Método para matricular o estudante, alterando o atributo 'matriculado' para True.
+    - aprovado(): Método para avançar o estudante para o próximo período, incrementando o atributo 'periodo' em 1.
+    - __str__(): Método especial para retornar uma string representando o estado atual do estudante.
+    """
+
     def __init__(self, nome = "", idade = 0, matriculado = False, curso = "", periodo = 1): #Método Construtor
+        #Documentação 
+        """
+        Docstring para __init__
+        
+        :param self: Própria classe
+        :param nome: Nome do Estudante
+        :param idade: Idade do estudante
+        :param matriculado: (True/False) Atributo para saber se o estudante esta ou não matriculado
+        :param curso: Nome do curso do estudante
+        :param periodo: Periodo em que o estudante está cursando
+        """
+
         #Atributos de instância
         self.nome = nome
         self.idade = idade
@@ -25,7 +54,7 @@ class Estudante():
 
 #Declaração de Objetos
 
-aluno1 = Estudante()
+aluno1 = Estudante("Eduardo")
 aluno1.nome = "Eduardo"
 aluno1.idade = 18
 aluno1.curso = "Engenharia da Computação"
@@ -38,3 +67,5 @@ aluno1.aprovado()
 aluno1.matricular()
 
 print(aluno1)
+
+print(Estudante.__doc__)
--- Testes.py
+++ Testes.py
@@ -1,20 +1,4 @@
-print("hello world")
-print("Notificação - Instanciar - Métodos - Estado - ")
-print(" Classe: celular")
-print("""\
-Atributos: 
-        Cor
-        Modelo
-        Tamanho
-""")
-print("""\
-Metodos:
-        Ligar()
-        Desligar()
-        Reiniciar()
-        Vibrar()
-""")
-
+print(int.__doc__)
 
 
 
